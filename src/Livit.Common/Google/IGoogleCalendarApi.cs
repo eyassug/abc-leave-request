e5be3564e8c3550d763b892501f5ef2c6389b22b
Eyassu Getachew <eyassug@gmail.com>
2017-02-08 03:09:19 +0300

    Added IGoogleCalendarApi signature for creating events, added helper class to create CalendarService from OAuth token

---
+++ src/Livit.Common/Google/IGoogleCalendarApi.cs
@@ -0,0 +1,15 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Livit.Common.Google
+{
+    public interface IGoogleCalendarApi
+    {
+        Task CreateEvent(CalendarService calendarService, Event @event, string calendarId);
+    }
+}
---
+++ src/Livit.Common/Google/UserCredentialInitializer.cs
@@ -0,0 +1,42 @@
+﻿using Google.Apis.Http;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Threading;
+using System.Net.Http.Headers;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+
+namespace Livit.Common.Google
+{
+    public class UserCredentialInitializer : IHttpExecuteInterceptor, IConfigurableHttpClientInitializer
+    {
+        private string _accessToken;
+
+        public UserCredentialInitializer(string accessToken)
+        {
+            _accessToken = accessToken;
+        }
+
+        public void Initialize(ConfigurableHttpClient httpClient)
+        {
+            httpClient.MessageHandler.AddExecuteInterceptor(this);
+        }
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public async Task InterceptAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _accessToken);
+        }
+
+        public static CalendarService CreateFromToken(string accessToken, string applicationName)
+        {
+            return new CalendarService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = new UserCredentialInitializer(accessToken),
+                ApplicationName = applicationName
+            });
+
+        }
+    }
+}
