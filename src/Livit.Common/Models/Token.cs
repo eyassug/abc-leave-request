6aead83cea66bb90608dbae5793f576b84b3e7d7
Eyassu Getachew <eyassug@gmail.com>
2017-02-09 17:06:47 +0300

    Added model for Token

---
+++ src/Livit.Common/Models/Token.cs
@@ -0,0 +1,20 @@
+﻿using ServiceStack.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Livit.Common.Models
+{
+    public class Token
+    {
+        [AutoIncrement]
+        public int Id { get; set; }
+        public string AccessToken { get; set; }
+        public string IdToken { get; set; }
+        public DateTime Issued { get; set; }
+        public string RefreshToken { get; set; }
+        public string Scope { get; set; }
+        public string TokenType { get; set; }
+    }
+}
